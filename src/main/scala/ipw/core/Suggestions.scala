68507d76c2bcbc55af89ac2fa63c86b2b2496e38
Roldak <romain-be@hotmail.com>
2017-04-26 03:41:56 +0200

    first idea of custom evaluator

--- src/main/scala/ipw/core/Suggestions.scala
+++ src/main/scala/ipw/core/Suggestions.scala
@@ -2,7 +2,7 @@ package ipw.core
 
 import inox.trees._
 import welder._
-import inox.evaluators.RecursiveEvaluator
+import ipw.utils.FunctionEvaluator
 import ipw.AssistedTheory
 
 trait Suggestions { theory: AssistedTheory =>
@@ -16,6 +16,8 @@ trait Suggestions { theory: AssistedTheory =>
 
   case class ExpandInvocation(val inv: FunctionInvocation) extends Suggestion(s"Expand invocation of ${inv.id}") {
     override def apply(e: Expr): Attempt[(Expr, Theorem)] = {
+      val evaluator = FunctionEvaluator.default(program)
+      println("MY EVAL: " + evaluator.eval(e))
       evaluated(e) flatMap { thm =>
         thm.expression match {
           case Equals(unev, ev) => (ev, thm)
